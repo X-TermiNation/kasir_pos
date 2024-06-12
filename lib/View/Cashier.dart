afa1dbb52bbedf1f45cddf11ed5453aca420187a
X-TermiNation <64397435+X-TermiNation@users.noreply.github.com>
2024-06-12 17:58:14 +0700

    initial commit

---
+++ lib/View/Cashier.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+
+class Cashier extends StatefulWidget {
+  const Cashier({super.key});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  @override
+  State<Cashier> createState() => _CashierState();
+}
+
+class _CashierState extends State<Cashier> {
+  final List<String> _items = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+    'Item 6',
+    'Item 7',
+    'Item 8',
+    'Item 9',
+    'Item 10',
+    'Item 11',
+    'Item 12',
+    'Item 13',
+    'Item 14',
+    'Item 15',
+    'Item 16',
+    'Item 17',
+    'Item 18',
+    'Item 19',
+    'Item 20',
+    'Item 21',
+    'Item 22',
+    'Item 23',
+    'Item 24',
+    'Item 25',
+  ];
+  List<String> _cart = [];
+  Map<String, int> _itemCounts = {};
+  List<String> _searchResults = [];
+  String _searchQuery = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('POS Cashier'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Search',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.search),
+              ),
+              onChanged: (query) {
+                setState(() {
+                  _searchQuery = query;
+                  _searchResults = _items
+                      .where((item) => item.toLowerCase().contains(query.toLowerCase()))
+                      .toList();
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: GridView.count(
+                    crossAxisCount: 4,
+                    children: (_searchQuery.isEmpty? _items : _searchResults).map((item) {
+                      return CardButton(
+                        onPressed: () {
+                          setState(() {
+                            if (_cart.contains(item)) {
+                              _itemCounts[item] = (_itemCounts[item]?? 0) + 1;
+                            } else {
+                              _cart.add(item);
+                              _itemCounts[item] = 1;
+                            }
+                          });
+                        },
+                        child: Text(item),
+                      );
+                    }).toList(),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: ListView.builder(
+                    itemCount: _cart.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(_cart[index]),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text("${_itemCounts[_cart[index]].toString()}x",style: TextStyle(fontSize: 15),),
+                            IconButton(
+                              icon: Icon(Icons.delete),
+                              onPressed: () {
+                                setState(() {
+                                  _itemCounts.remove(_cart[index]);
+                                  _cart.remove(_cart[index]);
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your submit logic here
+          print('Submit button pressed');
+        },
+        tooltip: 'Submit',
+        child: Text("Submit"),
+      ),
+    );
+  }
+}
+
+
+class CardButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final Widget child;
+
+  CardButton({required this.onPressed, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: InkWell(
+        onTap: onPressed,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: child,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/View/Login.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:kasir_pos/View/Cashier.dart';
+
+String idcabangglobal = "";
+String emailstr = "";
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login>{
+  TextEditingController email = new TextEditingController();
+  TextEditingController password = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text("Kasir"),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Kasir Pos',
+            ),
+            TextFormField(
+              controller: email,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Field tidak boleh kosong';
+                }
+                return null;
+              },
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Email',
+              ),
+            ),
+            TextFormField(
+              controller: password,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Field tidak boleh kosong';
+                }
+                return null;
+              },
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Password',
+              ),
+            ),
+            FilledButton(
+                onPressed: (){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Cashier()),
+                  );
+                }
+                ,child: Text("Login")
+            )
+          ],
+        ),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+}
